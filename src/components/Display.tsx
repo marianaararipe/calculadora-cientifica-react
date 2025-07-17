c176e6fcc740bad7b9cd8a682e1c569323cf6886
Mariana Araripe <marianaararipe07@gmail.com>
2025-07-17 12:46:43 -0300

    fix: números na expression ultrapassando o container
    feat: nova barra de rolagem na expression e nova função para scroll automático quando a expression é atualizada

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import styles from "./Display.module.css";
 
 type DisplayProps = {
@@ -6,9 +7,20 @@ type DisplayProps = {
 };
 
 export default function Display({ expression, result }: DisplayProps) {
+  const expressionRef = useRef<HTMLDivElement>(null);
+
+  // Função para scroll automático quando a  a expressão é atualizada
+  useEffect(() => {
+    if (expressionRef.current) {
+      expressionRef.current.scrollLeft = expressionRef.current.scrollWidth;
+    }
+  }, [expression]);
+
+
+
   return (
     <div className={styles.display}>
-      <div className={styles.expression}>{expression}</div>
+      <div className={styles.expression} ref={expressionRef}>{expression}</div>
       <div className={styles.result}>{result ? ` ${result}` : ""}</div>
     </div>
   );
